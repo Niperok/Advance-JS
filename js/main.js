2b7ea410648ae79833117449bd9c9f6103442919
korepin <niperok@yandex.ru>
2020-07-19 22:16:09 +0300

    Выполнил задание к уроку №1

--- js/main.js
+++ js/main.js
@@ -1,22 +1,24 @@
 const products = [
-    {id: 1, title: 'Notebook', price: 20000},
-    {id: 2, title: 'Mouse', price: 1500},
-    {id: 3, title: 'Keyboard', price: 5000},
-    {id: 4, title: 'Gamepad', price: 4500},
+    { id: 1, title: 'Notebook', price: 20000 },
+    { id: 2, title: 'Mouse', price: 1500 },
+    { id: 3, title: 'Keyboard', price: 5000 },
+    { id: 4, title: 'Gamepad', price: 4500 },
 ];
 
-const renderProduct = (title, price, img='') => {
+const renderProduct = ({ title = 'Product', price = 'XXXX', img = 'http://unsplash.it/150/150?random&gravity=center' }) => {
     return `<div class="product-item">
                 <h3>${title}</h3>
-                <img src="${img}" alt="">
+                <img src="${img}" alt="product">
                 <p>${price}</p>
                 <button class="by-btn">Добавить в корзину</button>
               </div>`;
 };
 
 const renderProducts = list => {
-    const productList = list.map(item => renderProduct(item.title, item.price));
-    console.log(productList);
+    let productList = '';
+    list.forEach(item => {
+        productList += renderProduct(item);
+    });
     document.querySelector('.products').innerHTML = productList;
 };
 
