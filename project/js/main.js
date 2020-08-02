3d7610c41659c48c48e7724d2e03f1dac93407de
korepin <niperok@yandex.ru>
2020-08-02 18:46:49 +0300

    Выполнил задания к уроку №5

--- project/js/main.js
+++ project/js/main.js
@@ -4,8 +4,14 @@ const app = new Vue({
   el: '#app',
   data: {
     catalogUrl: '/catalogData.json',
+    cartUrl: '/getBasket.json',
     products: [],
+    filtered: [],
+    productsInCart: [],
     imgCatalog: 'https://placehold.it/200x150',
+    imgCart: 'https://placehold.it/50x100',
+    searchLine: '',
+    isVisibleCart: false,
   },
   methods: {
     getJson(url){
@@ -17,18 +23,32 @@ const app = new Vue({
     },
     addProduct(product) {
       console.log(product);
+    },
+    filterGoods() {
+      const regexp = new RegExp(this.searchLine, 'i');
+      this.filtered = this.products.filter(product => regexp.test(product.product_name));
+    },
+    showCart() {
+      this.isVisibleCart = !this.isVisibleCart;
     }
   },
   beforeCreate() {
 
   },
   created() {
-      this.getJson(`${API + this.catalogUrl}`)
-          .then(data => {
-            for(let el of data){
-              this.products.push(el);
-            }
-          });
+    this.getJson(`${API + this.catalogUrl}`)
+        .then(data => {
+          for(let el of data){
+            this.products.push(el);
+          }
+          this.filtered = this.products;
+        });
+    this.getJson(`${API + this.cartUrl}`)
+        .then(data => {
+          for(let el of data.contents){
+            this.productsInCart.push(el);
+          }
+        });
   },
   beforeMount() {
 
