a48ddb93f983faab70be17020d07fbe6e4f5aec0
korepin <niperok@yandex.ru>
2020-08-09 17:17:26 +0300

    Исходники к заданию №7

--- public/js/CartComp.js
+++ public/js/CartComp.js
@@ -8,21 +8,21 @@ Vue.component('cart', {
       }
     },
     methods: {
+
         addProduct(product){
-            this.$parent.getJson(`${API}/addToBasket.json`)
-                .then(data => {
-                    if(data.result === 1){
-                        let find = this.cartItems.find(el => el.id_product === product.id_product);
-                        if(find){
-                            find.quantity++;
-                        } else {
-                            let prod = Object.assign({quantity: 1}, product);
-                            this.cartItems.push(prod)
-                        }
-                    } else {
-                        alert('Error');
-                    }
-                })
+            let find = this.cartItems.find(el => el.id_product === product.id_product);
+            if(find){
+                this.$parent.putJson(`/api/cart/${find.id_product}`, {quantity: 1});
+                find.quantity++;
+            } else {
+                let prod = Object.assign({quantity: 1}, product);
+                this.$parent.postJson('/api/cart', prod)
+                  .then(data => {
+                      if (data.result === 1) {
+                          this.cartItems.push(prod);
+                      }
+                  });
+            }
         },
         remove(item) {
             this.$parent.getJson(`${API}/deleteFromBasket.json`)
@@ -65,18 +65,18 @@ Vue.component('cart-item', {
     props: ['cartItem', 'img'],
     template: `
                 <div class="cart-item">
-                    <div class="product-bio">
-                        <img :src="img" alt="Some image">
-                        <div class="product-desc">
-                            <p class="product-title">{{cartItem.product_name}}</p>
-                            <p class="product-quantity">Количество: {{cartItem.quantity}}</p>
-                            <p class="product-single-price">{{cartItem.price}}₽ за единицу</p>
-                        </div>
-                    </div>
-                    <div class="right-block">
-                        <p class="product-price">{{cartItem.quantity*cartItem.price}}₽</p>
-                        <button class="del-btn" @click="$emit('remove', cartItem)">&times;</button>
+                <div class="product-bio">
+                    <img :src="img" alt="Some image">
+                    <div class="product-desc">
+                        <p class="product-title">{{cartItem.product_name}}</p>
+                        <p class="product-quantity">Количество: {{cartItem.quantity}}</p>
+                        <p class="product-single-price">{{cartItem.price}}₽ за единицу</p>
                     </div>
                 </div>
+                <div class="right-block">
+                    <p class="product-price">{{cartItem.quantity*cartItem.price}}₽</p>
+                    <button class="del-btn" @click="$emit('remove', cartItem)">&times;</button>
+                </div>
+            </div>
     `
 });
